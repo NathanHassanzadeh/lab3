678775bd8cfce82a1b9154593057bf80d42d2542
ehassanz@ucsd.edu <nathan@Nathans-MacBook-Pro.local>
2018-01-24 21:01:29 -0800

    Added projectClick function

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -16,3 +16,12 @@ function initializePage() {
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
+function projectClick(e) {
+	e.preventDefault();
+	$this.css("backgrond-color", "#7fff00");
+
+	var containingProject = $(this).closest(".project");
+    containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+}
+
+}
