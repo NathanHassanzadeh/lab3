a98c9e33a018911cda23357e979b9abbf4416e42
ehassanz@ucsd.edu <nathan@Nathans-MacBook-Pro.local>
2018-01-25 10:29:09 -0800

    Made updates to js

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,18 +10,26 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
 }
+
 function projectClick(e) {
 	e.preventDefault();
-	$this.css("backgrond-color", "#7fff00");
-
+	// console.log("Project Clicked")
+	$(this).css("background-color", "#7fff00");
 	var containingProject = $(this).closest(".project");
-    containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
-}
-
+	var description = $(containingProject).find(".project-description")
+	if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+       $(description).fadeOut()
+    }
 }
